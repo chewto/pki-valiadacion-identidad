2539dbf250a9295e54f415e7c879565c2242a222
jesus lozada <jesuselozada@gmail.com>
2023-10-17 13:12:47 -0400

    conflictos arreglados

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ValidacionIdentidad } from "./paginas/validacion-identidad";
 
@@ -35,7 +11,6 @@ function App() {
         </Routes>
       </BrowserRouter>
     </>
->>>>>>> camara-nueva
   );
 }
 
